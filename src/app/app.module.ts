b1eeb07e0f5e8b09171475471f2e79fd186f2e95
Gimpan <jimmyquach@live.com>
2018-12-28 13:59:48 +0100

    Added more routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,6 +19,8 @@ import { DropdownDirective } from './shared/dropdown.directive';
 import { ShoppingListService } from './shopping-list/shopping-list.service';
 import { AppRoutingModule } from './app-routing.module';
 import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
+import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
+import { NumbersgameComponent } from './shared/numbersGame/numbersgame/numbersgame.component';
 
 @NgModule({
   declarations: [
@@ -35,7 +37,9 @@ import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.compon
     EavesdropperDirective,
     UnlessDirective,
     DropdownDirective,
-    RecipeStartComponent
+    RecipeStartComponent,
+    RecipeEditComponent,
+    NumbersgameComponent
   ],
   imports: [
     BrowserModule,
