4fb6aa24ddf4194303e635da36e96100a9f86a37
Gimpan <jimmyquach@live.com>
2019-01-02 10:38:58 +0100

    Added comment

--- src/app/shared/shorten.pipe.ts
+++ src/app/shared/shorten.pipe.ts
@@ -2,6 +2,7 @@ import { PipeTransform, Pipe } from '@angular/core';
 
 @Pipe({
   name: 'shorten'
+  // pure: false , performance cost for enabling live filtering
 })
 export class ShortenPipe implements PipeTransform {
   transform(value: any) {
