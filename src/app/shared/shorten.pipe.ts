b6e4fb49d7abc4c251b9f65016fd12001db42ce3
Gimpan <jimmyquach@live.com>
2019-01-02 10:19:30 +0100

    Fixed bug on shorten pipe

--- src/app/shared/shorten.pipe.ts
+++ src/app/shared/shorten.pipe.ts
@@ -5,7 +5,7 @@ import { PipeTransform, Pipe } from '@angular/core';
 })
 export class ShortenPipe implements PipeTransform {
   transform(value: any) {
-    if (value > 10) {
+    if (value.length > 10) {
     return value.substr(0, 10) + ' ...';
     }
     return value;
