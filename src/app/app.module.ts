83219558d8f7c0ff7c1a8ec74bf81c49f86f2b11
Gimpan <jimmyquach@live.com>
2019-01-03 15:47:03 +0100

    Added guards

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -26,6 +26,7 @@ import { DataStorageService } from './shared/data-storage.service';
 import { SignupComponent } from './auth/signup/signup.component';
 import { SigninComponent } from './auth/signin/signin.component';
 import { AuthService } from './auth/auth.service';
+import { AuthGuard } from './auth/auth-guard.service';
 
 @NgModule({
   declarations: [
@@ -55,7 +56,7 @@ import { AuthService } from './auth/auth.service';
     HttpModule,
     AppRoutingModule
   ],
-  providers: [ShoppingListService, RecipeService, DataStorageService, AuthService],
+  providers: [ShoppingListService, RecipeService, DataStorageService, AuthService, AuthGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
